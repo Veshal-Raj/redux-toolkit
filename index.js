2af5debe66b0d6e42309e78d078e528653813466
Veshal Raj <veshalraj1307@gmail.com>
2023-11-27 12:33:32 +0530

    extra reducers

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 console.log('Inital State', store.getState())
-const unsubscribe = store.subscribe(() => {})
+const unsubscribe = store.subscribe(() => {
+    console.log('updated state ',store.getState())
+})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
