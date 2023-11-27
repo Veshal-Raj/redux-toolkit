865652affe342c745fddac494ef7f49837d3957f
Veshal Raj <veshalraj1307@gmail.com>
2023-11-27 12:12:44 +0530

    logger

--- app/store.js
+++ app/store.js
@@ -1,12 +1,17 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
+const { getDefaultMiddleware } = require('@reduxjs/toolkit')
+
+const logger = reduxLogger.createLogger()
 
 const store = configureStore({
     reducer: {
         cakes: cakeReducer,
         icecream: icecreamReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 module.exports = store
--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 console.log('Inital State', store.getState())
-const unsubscribe = store.subscribe(() => {
-    console.log('updated state ', store.getState())
-})
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
